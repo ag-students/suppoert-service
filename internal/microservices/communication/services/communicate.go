251505bf2d1431bebbee39e5810003e3275dc574
Тит Дасов <tetovske@yandex.ru>
2021-07-07 03:50:39 +0300

    [WIP] linters setup

--- internal/microservices/communication/services/communicate.go
+++ internal/microservices/communication/services/communicate.go
@@ -1,9 +1,18 @@
 package services
 
-import "github.com/ag-students/support-service/internal/microservices/communication/repository"
+import (
+	"github.com/ag-students/support-service/internal/microservices/communication/repository"
+	"log"
+)
 
 type PatientNotifier struct {
 	repo *repository.Repository
+}
+
+func (r *PatientNotifier) Notify() error {
+	log.Print(r.repo)
+
+	return nil
 }
 
 
