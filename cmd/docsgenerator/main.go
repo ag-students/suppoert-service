3a3939a638039ee17b551902fc0b961778a02c18
Владислав <pushkin_vladislav@mail.ru>
2021-07-13 14:44:18 +0300

    added call function

--- cmd/docsgenerator/main.go
+++ cmd/docsgenerator/main.go
@@ -7,5 +7,10 @@ import (
 func main() {
 
 	fmt.Println("Hello, World! I generate docs")
+
+	surname := "Иванов"
+	name := "Иван"
+	patronymic := "Иванович"
+ 	createPDF(surname, name, patronymic)
 	
 }
