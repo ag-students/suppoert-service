a64565cf9bd6cbcf7d1301a1fe2d56dff4fc2e64
Владислав <pushkin_vladislav@mail.ru>
2021-07-16 13:33:03 +0300

    fixed problems with createPDF

--- pkg/pdf-creator/createPDF.go
+++ pkg/pdf-creator/createPDF.go
@@ -7,23 +7,17 @@ import (
 )
 
 func CreatePDF(surname, name, patronymic string) *gofpdf.Fpdf {
-
-	err := os.Remove("passport.pdf")
-	if err != nil {
-		return nil
-	}
-
 	pwd, err1 := os.Getwd()
 	if err1 != nil {
 		fmt.Println(err1)
 	}
 
-	pdf := gofpdf.New("L", "mm", "A4", pwd+"/font")
+	pdf := gofpdf.New("L", "mm", "A4", pwd+"/pkg/pdf-creator/font")
 
 	pdf.AddFont("Helvetica", "", "helvetica_1251.json")
 
 	pdf.AddPage()
-	pdf.Image("passport.png", 0, 0, 298, 0, false, "", 0, "")
+	pdf.Image("pkg/pdf-creator/passport.png", 0, 0, 298, 0, false, "", 0, "")
 	pdf.SetFont("Helvetica", "", 18)
 	tr := pdf.UnicodeTranslatorFromDescriptor("cp1251")
 
@@ -42,9 +36,13 @@ func CreatePDF(surname, name, patronymic string) *gofpdf.Fpdf {
 	pdf.SetFont("Helvetica", "", 13)
 	pdf.SetXY(45, 212)
 	pdf.Cell(20, -23, tr("Не явлется официальным документом! Разработано исключительно в образовательных целях"))
-	err = pdf.OutputFileAndClose("passport.pdf")
+	err := pdf.OutputFileAndClose("passport.pdf")
 	if err != nil {
 		fmt.Println("⚠️  Could not save PDF:", err)
+	} else {
+		fmt.Println("Генерация прошла успешно Файл лежит в контейнере!")	
+		// docker cp <container_name>:/support-service/passport.pdf ./
+		// чтобы достать файл из контейнера
 	}
 	return pdf
 }
