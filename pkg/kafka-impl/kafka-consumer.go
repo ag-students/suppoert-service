ebc3fdf07a919b506f51a95ec2d4265111924c82
brshpl <igor_barishpol@mail.ru>
2021-07-18 20:30:05 +0300

    Added handler for message

--- pkg/kafka-impl/kafka-consumer.go
+++ pkg/kafka-impl/kafka-consumer.go
@@ -1,12 +1,21 @@
 package kafka_impl
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
+	pdf_creator "github.com/ag-students/support-service/pkg/pdf-creator"
 	"github.com/segmentio/kafka-go"
 	"log"
 	"strings"
 )
+
+type DataForDocument struct {
+	surname    string
+	name       string
+	patronymic string
+}
 
 func GetKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
 	brokers := strings.Split(kafkaURL, ",")
@@ -19,6 +28,24 @@ func GetKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
 	})
 }
 
+func handleMessage(message kafka.Message) error {
+	if string(message.Key) == "DataForDocument" {
+		data := DataForDocument{}
+		buf := &bytes.Buffer{}
+		buf.Write(message.Value)
+		err := binary.Read(buf, binary.BigEndian, &data)
+		if err != nil {
+			log.Fatalf("Failed to convert message '%s' to struct", message.Key)
+			return err
+		} else {
+			log.Printf("Key: %s\nValue: %s\n", message.Key, data)
+			pdf_creator.CreatePDF(data.surname, data.name, data.patronymic)
+			return nil
+		}
+	}
+	return nil
+}
+
 // Listen TODO: добавить обработчик пришедшего запроса
 func Listen(ctx context.Context, reader *kafka.Reader) {
 	fmt.Println("start consuming ... !!")
@@ -26,6 +53,10 @@ func Listen(ctx context.Context, reader *kafka.Reader) {
 		m, err := reader.ReadMessage(ctx)
 		if err != nil {
 			log.Fatalln(err)
+		}
+		err = handleMessage(m)
+		if err != nil {
+			log.Fatalf("Probably, value of message '%s' isn't correct", m.Key)
 		}
 		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
 	}
